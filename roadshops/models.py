fe6404cdd6b035cdf69c7a6d1e8fb09052752928
ldy <feb29dy@gmail.com>
2015-12-09 21:07:03 +0900

    init

---
+++ roadshops/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Roadshops(models.Model):
+    ETUDE = 'ETUDE_HOUSE'
+    THE_FACE_SHOP = 'THE_FACE_SHOP'
+    SKIN_FOOD = 'SKIN_FOOD'
+    MISSHA = 'MISSHA'
+
+    ROADSHOP_CHOICE = (
+        (ETUDE, 'ETUDE_HOUSE'),
+        (THE_FACE_SHOP, 'THE_FACE_SHOP'),
+        (SKIN_FOOD, 'SKIN_FOOD'),
+        (MISSHA, 'MISSHA'),
+    )
+
+    event_name = models.CharField(max_length=200)
+    roadshop_name = models.CharField(max_length=20, choices=ROADSHOP_CHOICE)
+
+    describe = models.CharField(max_length=300, null=True, blank="")
+    image_url = models.CharField(max_length=300, null=True, blank="")
+    link_url = models.CharField(max_length=300, null=True, blank="")
+
+    start_date = models.DateTimeField(null=True, blank="")
+    end_date = models.DateTimeField(null=True, blank="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ roadshops/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import TemplateView
+from django.shortcuts import render_to_response
+from roadshops.models import Roadshops
+from datetime import datetime
+
+import urllib.request
+
+
+class Home(TemplateView):
+    template_name = 'main.html'
+
+
+def home(request, num=None, num2=None):
+    context = {'num': num, 'num2': num2}
+    return render_to_response('main.html', context)
+
+
+def temp(request):
+    Roadshops.objects.all().delete()
+    etude()
+    context = print_etude()
+
+    return render_to_response('temp.html', context)
+
+
+def etude():
+    url = 'http://www.etude.co.kr/event.do?method=list'
+    f = urllib.request.urlopen(url)
+    text = f.read().decode('utf-8')
+
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(text)
+
+    templist = soup.find('div', 'evnetList')
+    ul_tag = templist.contents[1]
+
+    eventlist = []
+
+    for event in ul_tag.contents:
+        if event != '\n':
+            eventlist.append(event)
+
+    for event in eventlist:
+        temp = Roadshops()
+
+        period = event.find('em').get_text()
+        start_date = period.split(' ~ ')[0]
+        end_date = period.split(' ~ ')[1]
+
+        temp.event_name = event.find('span', 'tit').get_text()
+        temp.roadshop_name = Roadshops.ETUDE
+        temp.image_url = event.find('img').get('src')
+        temp.link_url = event.find('a').get('href')
+        temp.start_date = datetime.strptime(start_date, "%Y.%m.%d")
+        temp.end_date = datetime.strptime(end_date + " 23:59:59", "%Y.%m.%d %H:%M:%S")
+        temp.save()
+
+
+def skinfood():
+
+    url = 'http://www.theskinfood.com/event/eventList.do'
+    f = urllib.request.urlopen(url)
+    text = f.read().decode('utf-8')
+
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(text)
+
+    templist = soup.find('ul', 'eventList passed')
+
+    eventlist = []
+
+    for event in templist.contents:
+        if event != '\n':
+            eventlist.append(event)
+
+    for event in eventlist:
+        temp = Roadshops()
+
+        # 혜택, 기간 나누기
+        period = event.find('p').get_text()
+        start_date = period.split(' ~ ')[0]
+        end_date = period.split(' ~ ')[1]
+
+        temp.event_name = event.find('strong').get_text()
+        temp.roadshop_name = 'SKIN_FOOD'
+        temp.image_url = event.find('img').get('src')
+        temp.link_url = event.find('a').get('href')
+        temp.start_date = datetime.strptime(start_date, "%Y.%m.%d")
+        temp.end_date = datetime.strptime(end_date + " 23:59:59", "%Y.%m.%d %H:%M:%S")
+        temp.save()
+
+
+def print_etude():
+    shops = Roadshops.objects.all()
+    context = {'shops': shops}
+    return context
