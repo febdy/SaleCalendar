307b6e8dce046cb0510efa571d8257ecf18fdb57
ldy <feb29dy@gmail.com>
2016-01-02 23:52:21 +0900

    SKINFOOD added

--- roadshops/views.py
+++ roadshops/views.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
 from roadshops.models import Roadshops
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 import urllib.request
 
@@ -17,14 +18,16 @@ class Home(TemplateView):
 def home(request, num=None, num2=None):
     Roadshops.objects.all().delete()
     etude()
-    context = print_etude()
+    skinfood()
+    context = print_shops()
     return render_to_response('main.html', context)
 
 
 def temp(request):
     Roadshops.objects.all().delete()
     etude()
-    context = print_etude()
+    skinfood()
+    context = print_shops()
 
     return render_to_response('temp.html', context)
 
@@ -34,7 +37,6 @@ def etude():
     f = urllib.request.urlopen(url)
     text = f.read().decode('utf-8')
 
-    from bs4 import BeautifulSoup
     soup = BeautifulSoup(text)
 
     templist = soup.find('div', 'evnetList')
@@ -56,19 +58,20 @@ def etude():
         temp.event_name = event.find('span', 'tit').get_text()
         temp.roadshop_name = Roadshops.ETUDE
         temp.image_url = event.find('img').get('src')
-        temp.link_url = event.find('a').get('href')
+        if event.find('a').get('href')[0] == '/':
+            temp.link_url = 'http://www.etude.co.kr'+event.find('a').get('href')
+        elif event.find('a').get('href')[0] != '/':
+            temp.link_url = event.find('a').get('href')
         temp.start_date = datetime.strptime(start_date, "%Y.%m.%d")
         temp.end_date = datetime.strptime(end_date + " 23:59:59", "%Y.%m.%d %H:%M:%S")
         temp.save()
 
 
 def skinfood():
-
     url = 'http://www.theskinfood.com/event/eventList.do'
     f = urllib.request.urlopen(url)
     text = f.read().decode('utf-8')
 
-    from bs4 import BeautifulSoup
     soup = BeautifulSoup(text)
 
     templist = soup.find('ul', 'eventList passed')
@@ -90,13 +93,13 @@ def skinfood():
         temp.event_name = event.find('strong').get_text()
         temp.roadshop_name = 'SKIN_FOOD'
         temp.image_url = event.find('img').get('src')
-        temp.link_url = event.find('a').get('href')
-        temp.start_date = datetime.strptime(start_date, "%Y.%m.%d")
-        temp.end_date = datetime.strptime(end_date + " 23:59:59", "%Y.%m.%d %H:%M:%S")
+        temp.link_url = 'http://www.theskinfood.com'+event.find('a').get('href')
+        temp.start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        temp.end_date = datetime.strptime(end_date + " 23:59:59", "%Y-%m-%d %H:%M:%S")
         temp.save()
 
 
-def print_etude():
+def print_shops():
     shops = Roadshops.objects.all()
     context = {'shops': shops}
     return context
