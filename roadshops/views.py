060226254ba2d6a49994f8d1704832def3b9f18e
ldy <feb29dy@gmail.com>
2016-06-16 01:29:28 +0900

    fixed - the calendar shows all events. It showed only active events before.

--- roadshops/views.py
+++ roadshops/views.py
@@ -24,20 +24,7 @@ def temp(request):
     return render_to_response('temp.html', context)
 
 
-def check_event_active():
-    db_eventlist = Roadshops.objects.all().filter(is_active='Y')
-
-    for db_event in db_eventlist:
-        now = datetime.now(timezone.utc)
-        last_updated = db_event.updated_at
-
-        if now - last_updated > timedelta(hours=24):
-            db_event.is_active = 'N'
-            db_event.save()
-
-
 def print_shops():
-    check_event_active()
-    shops = Roadshops.objects.all().filter(is_active='Y')
+    shops = Roadshops.objects.all()
     context = {'shops': shops}
     return context
